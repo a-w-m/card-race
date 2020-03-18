65ab66241a8c6623993332d12dd42a94f470b5e1
a-w-m <47095419+mamrey@users.noreply.github.com>
2020-03-17 23:03:14 -0400

    fix merge conflict

--- src/components/card.js
+++ src/components/card.js
@@ -5,31 +5,6 @@ import styles from "./card.module.css"
 const Card = props => {
   const { className, id, handleClick, style} = props
 
-<<<<<<< HEAD
-    
-  const {className, id, onClick, style, ref} = props;
- 
-  const [flip, setFlip] = useState({})
-
-
-  const handleClick = useCallback((card) => {    
- onClick(card)
-
-}, [onClick])
-
-      
- 
-  
-
-  return (
-    <div className = {styles.scene} onClick ={()=>handleClick({"class":className, id: id })} >
-    <div className = {styles.card} style = {style} ref ={ref}>
-    <div id = {id} className = {`${styles.cardFace} ${styles.cardFaceFront}`} ></div>
-    <div className = {`${styles.cardFace} ${styles.cardFaceBack}`}>
-        <Icon className ={className}></Icon>
-    </div>
-    </div>
-=======
   const handleClickCard = card => {
     if (style.transform === "rotateY(0deg)"){
     handleClick(card)
@@ -50,7 +25,6 @@ const Card = props => {
           <Icon className={className}></Icon>
         </div>
       </div>
->>>>>>> flip
     </div>
   )
 }
