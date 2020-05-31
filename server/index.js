a6a3c10326f0b672324ad7b180aaea27beefa49b
a-w-m <mamrey@gmail.com>
2020-05-30 20:08:57 -0400

    Fix Static Folder Root Path

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use("/api/scores", scoresRouter)
     app.use(express.static('public/'))
 
     app.get('*', (req, res) => {
-      res.sendFile(__dirname +'/public/index.html');
+      res.sendFile(__dirname +'/../public/index.html');
     });
   
 
